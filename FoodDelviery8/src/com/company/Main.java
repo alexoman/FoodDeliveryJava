b338a3bfd9f00f08379c7c5ff82b905be2315f1f
alexoman <57303022+alexoman@users.noreply.github.com>
2021-07-04 17:04:19 +0300

    Add files via upload

---
+++ FoodDelviery8/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.company.Anything.*;
+import com.company.Personal.*;
+import com.company.Places.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException{
+
+    }
+}
